7f1dcf5c10dce9d7fb44f1e516dff5c3f63fc1d7
zony21 <38487257+zony21@users.noreply.github.com>
2026-09-16 12:03:18 +0900

    feat: mount daily report review and validation tools

--- src/Root.vue
+++ src/Root.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import App from './App.vue'
 import WeeklyReportTool from './WeeklyReportTool.vue'
+import DailyReportReviewTool from './DailyReportReviewTool.vue'
+import DailyReportValidationGuard from './DailyReportValidationGuard.vue'
 </script>
 
 <template>
   <App />
   <WeeklyReportTool />
+  <DailyReportReviewTool />
+  <DailyReportValidationGuard />
 </template>
