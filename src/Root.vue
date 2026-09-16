edc8e6d7437c9d8d90aa9310174e0eb5e259a7e6
zony21 <38487257+zony21@users.noreply.github.com>
2026-09-16 13:19:03 +0900

    feat: connect daily review navigation flow

--- src/Root.vue
+++ src/Root.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import App from './App.vue'
-import WeeklyReportTool from './WeeklyReportTool.vue'
+import DailyReportNavigationBridge from './DailyReportNavigationBridge.vue'
 import DailyReportReviewTool from './DailyReportReviewTool.vue'
+import WeeklyReportTool from './WeeklyReportTool.vue'
 import DailyReportValidationGuard from './DailyReportValidationGuard.vue'
 </script>
 
 <template>
   <App />
-  <WeeklyReportTool />
+  <DailyReportNavigationBridge />
   <DailyReportReviewTool />
+  <WeeklyReportTool />
   <DailyReportValidationGuard />
 </template>
