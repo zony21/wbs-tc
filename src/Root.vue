233e33e8079160f92abb9d71c9a389225e184d05
zony21 <38487257+zony21@users.noreply.github.com>
2026-09-16 14:26:11 +0900

    feat: wire section edit and daily draft tools

--- src/Root.vue
+++ src/Root.vue
@@ -5,6 +5,8 @@ import DailyReportReviewTool from './DailyReportReviewTool.vue'
 import WeeklyReportTool from './WeeklyReportTool.vue'
 import DailyReportValidationGuard from './DailyReportValidationGuard.vue'
 import ScheduleTaskEnhancer from './ScheduleTaskEnhancer.vue'
+import SectionEditTool from './SectionEditTool.vue'
+import DailyDraftEditTool from './DailyDraftEditTool.vue'
 </script>
 
 <template>
@@ -14,4 +16,6 @@ import ScheduleTaskEnhancer from './ScheduleTaskEnhancer.vue'
   <WeeklyReportTool />
   <DailyReportValidationGuard />
   <ScheduleTaskEnhancer />
+  <SectionEditTool />
+  <DailyDraftEditTool />
 </template>
