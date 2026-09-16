2f755bc65e319468370687977becca71fb4ae5f6
zony21 <38487257+zony21@users.noreply.github.com>
2026-09-16 11:56:40 +0900

    feat: bootstrap app state from SQLite

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import Root from './Root.vue'
+import { bootstrapDatabaseState, startDatabaseSync } from './dbSync'
 import './style.css'
 
-createApp(App).mount('#app')
+await bootstrapDatabaseState()
+createApp(Root).mount('#app')
+startDatabaseSync()
